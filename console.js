a599acda9745bce91643abd5532a0e169b95d0ab
Iacovos Constantinou <softius@gmail.com>
2015-08-02 22:59:57 +0300

    Add daemon mode

--- console.js
+++ console.js
@@ -5,7 +5,7 @@ var colors = require('colors')
 var optimist = require('optimist')
 var argv = optimist
 	.usage('Usage: $0')
-	// Help 
+	// Help
 	.default('h')
 	.alias('h', 'help')
     .describe('h', 'Print this help')
@@ -13,6 +13,10 @@ var argv = optimist
 	.default('c', 'config.json')
 	.alias('c', 'config')
     .describe('c', 'Specify the configuration file')
+    // Interval in seconds
+	.default('d')
+	.alias('d', 'daemon')
+     .describe('d', 'Run as a daemon')
 	.argv
 
 // Print help screen
@@ -49,17 +53,4 @@ statis.on('result', function(result) {
 statis.registerNotificationListeners(nconf.get('notifications'))
 
 // Do the work
-function statis_analysis()
-{
-	for (var i=0; i<nodes.length; i++) {
-		if (nodes[i].http)
-			statis.http(nodes[i])
-
-		if (nodes[i].socket)
-			statis.socket(nodes[i])
-
-		if (nodes[i].mysql)
-			statis.mysql(nodes[i])
-	}
-}
-statis_analysis()
+statis.scan(nodes, argv.d)
